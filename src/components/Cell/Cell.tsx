12c7ed868f085b464d3423c5b51fbcf0abe46f61
Konstantin Gadyrka <kgadyrka@340basics.com>
2024-02-04 02:36:27 +0300

    horizontal merge logic implemented

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,14 +1,10 @@
 import { CellDefinition } from '@/components/Field/useField.ts';
 import clsx from 'clsx';
 import styles from './Cell.module.scss';
-import { useMountEffect } from '@react-hookz/web';
 
 type CellProps = CellDefinition;
 
-export const Cell = ({ id, row, column, visible, value, merged }: CellProps) => {
-    useMountEffect(() => {
-        console.log('Cell mounted ' + id);
-    });
+export const Cell = ({ row, column, visible, value, merged }: CellProps) => {
     return (
         <div
             className={clsx(
--- src/components/Field/useField.ts
+++ src/components/Field/useField.ts
@@ -26,6 +26,13 @@ type FieldRow = (FieldCell | null)[];
 type Field = FieldRow[];
 type CellDefinitionMap = Record<string, CellDefinition>;
 
+const KEY_LEFT = 'ArrowLeft';
+const KEY_RIGHT = 'ArrowRight';
+const KEY_UP = 'ArrowUp';
+const KEY_DOWN = 'ArrowDown';
+
+const arrowKeysSet = new Set([KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN]);
+
 //todo: move inside hook?
 let id = 5;
 const getNewId = () => id++;
@@ -42,34 +49,36 @@ export const useField = (fieldSize: number) => {
     useKeyboardEvent(
         true,
         (event) => {
+            if (!arrowKeysSet.has(event.key)) {
+                return;
+            }
+
             const currentCellMap = prepareFieldForMove(cellMap);
             const field = cellMapToField(currentCellMap, fieldSize);
 
+            let newCellMap: CellDefinitionMap;
             switch (event.key) {
-                case 'ArrowLeft': {
-                    const newCellMap = onMoveLeft(field);
-                    updateCellMap(newCellMap, currentCellMap, setCellMap);
+                case KEY_LEFT:
+                    newCellMap = onMoveHorizontalLeft(field, false);
                     break;
-                }
-                case 'ArrowRight':
-                    console.log('RIGHT');
+                case KEY_RIGHT:
+                    newCellMap = onMoveHorizontal(field, true);
                     break;
-                case 'ArrowUp':
-                    console.log('UP');
+                case KEY_UP:
+                    newCellMap = onMoveVertical(field, false);
                     break;
-                case 'ArrowDown':
-                    console.log('DOWN');
+                case KEY_DOWN:
+                    newCellMap = onMoveVertical(field, true);
                     break;
                 default:
-                    break;
+                    return;
             }
+            updateCellMap(newCellMap, currentCellMap, setCellMap);
         },
         []
     );
 
-    console.log(Object.values(cellMap));
-
-    return { idCellMap: cellMap, onMoveLeft };
+    return { idCellMap: cellMap, onMoveLeft: onMoveHorizontal };
 };
 
 const updateCellMap = (
@@ -103,47 +112,116 @@ const prepareFieldForMove = (cellMap: CellDefinitionMap): CellDefinitionMap =>
         .keyBy((cell) => cell.id)
         .value();
 
-const onMoveLeft = (field: Field): CellDefinitionMap => {
-    const effectiveField = makeEmptyField(field.length);
-    for (let row = 0; row < field.length; row++) {
-        for (let column = 0; column < field.length; column++) {
-            const cell = field[row][column];
-            if (cell) {
-                const newCell = getNewDefinitionLeft(effectiveField[row], cell);
-                effectiveField[row][newCell.visibleCell.column] = newCell;
-            }
-        }
-    }
+// const onMoveHorizontal = (field: Field, isForward: boolean): CellDefinitionMap => {
+//     const effectiveField = makeEmptyField(field.length);
+//     for (let row = 0; row < field.length; row++) {
+//         for (let column = 0; column < field.length; column++) {
+//             const cell = field[row][column];
+//             if (cell) {
+//                 const newCell = getNewDefinition(effectiveField[row], cell, isForward);
+//                 effectiveField[row][newCell.visibleCell.column] = newCell;
+//             }
+//         }
+//     }
+//     return fieldToCellMap(effectiveField);
+// };
 
+const onMoveHorizontalLeft = (field: Field, isForward: boolean): CellDefinitionMap => {
+    const effectiveField = field.reduce((resultField, row, rowIndex) => {
+        const reduceFn = reduce;
+        const reducedRow = reduceFn(
+            row,
+            (resultRow, cell) => {
+                if (cell) {
+                    const newCell = getNewDefinitionLeft(resultRow, cell, isForward);
+                    resultRow[newCell.visibleCell.column] = newCell;
+                }
+                return resultRow;
+            },
+            makeEmptyFieldRow(field.length)
+        );
+        resultField[rowIndex] = reducedRow;
+        return resultField;
+    }, makeEmptyField(field.length));
     return fieldToCellMap(effectiveField);
 };
 
-const getNewDefinitionLeft = (effectiveFieldRow: FieldRow, cell: FieldCell): FieldCell =>
-    reduceRight(
-        effectiveFieldRow.slice(0, cell.visibleCell.column),
-        (effectiveCell, leftCell, index) => {
-            if (!leftCell) {
+const onMoveHorizontal = (field: Field, isForward: boolean): CellDefinitionMap => {
+    const effectiveField = field.reduce((resultField, row, rowIndex) => {
+        const reduceFn = reduceRight;
+        const reducedRow = reduceFn(
+            row,
+            (resultRow, cell) => {
+                if (cell) {
+                    const newCell = getNewDefinition(resultRow, cell, isForward);
+                    resultRow[newCell.visibleCell.column] = newCell;
+                }
+                return resultRow;
+            },
+            makeEmptyFieldRow(field.length)
+        );
+        resultField[rowIndex] = reducedRow;
+        rowIndex === 0 && console.log(reducedRow);
+        return resultField;
+    }, makeEmptyField(field.length));
+    return fieldToCellMap(effectiveField);
+};
+
+const onMoveVertical = (field: Field, isForward: boolean): CellDefinitionMap => {
+    const effectiveField = makeEmptyField(field.length);
+    for (let column = 0; column < field.length; column++) {
+        for (let row = 0; row < field.length; row++) {
+            const cell = field[row][column];
+            if (cell) {
+                const newCell = getNewDefinition(
+                    effectiveField.map((row) => row[column]),
+                    cell,
+                    isForward
+                );
+                effectiveField[newCell.visibleCell.row][column] = newCell;
+            }
+        }
+    }
+    return fieldToCellMap(effectiveField);
+};
+
+const getNewDefinition = (effectiveFieldRow: FieldRow, cell: FieldCell, isForward: boolean): FieldCell => {
+    const reduceFn = reduce;
+    const effectiveRowSegment = effectiveFieldRow.slice(cell.visibleCell.column + 1);
+    return reduceFn(
+        effectiveRowSegment,
+        (effectiveCell, nextCell, index) => {
+            const nextCellIndex = effectiveFieldRow.length - (effectiveRowSegment.length - index);
+            console.log(
+                'getNewDefinition',
+                effectiveCell,
+                nextCell,
+                nextCellIndex,
+                effectiveFieldRow,
+                effectiveRowSegment
+            );
+            if (!nextCell) {
                 return {
                     visibleCell: {
                         ...effectiveCell.visibleCell,
-                        column: index
+                        column: nextCellIndex
                     },
-                    hiddenCells: effectiveCell.hiddenCells.map((cell) => ({ ...cell, column: index }))
+                    hiddenCells: effectiveCell.hiddenCells.map((cell) => ({ ...cell, column: nextCellIndex }))
                 };
-            } else if (!leftCell.visibleCell.merged && leftCell.visibleCell.value === cell.visibleCell.value) {
+            } else if (!nextCell.visibleCell.merged && nextCell.visibleCell.value === effectiveCell.visibleCell.value) {
                 return {
                     visibleCell: {
                         ...effectiveCell.visibleCell,
                         id: getNewId(),
-                        column: index,
-                        value: cell.visibleCell.value * 2,
+                        column: nextCellIndex,
+                        value: effectiveCell.visibleCell.value * 2,
                         merged: true
                     },
                     hiddenCells: [
-                        leftCell.visibleCell,
+                        nextCell.visibleCell,
                         {
                             ...effectiveCell.visibleCell,
-                            column: index
+                            column: nextCellIndex
                         }
                     ]
                 };
@@ -152,6 +230,46 @@ const getNewDefinitionLeft = (effectiveFieldRow: FieldRow, cell: FieldCell): Fie
         },
         cloneDeep(cell)
     );
+};
+
+const getNewDefinitionLeft = (effectiveFieldRow: FieldRow, cell: FieldCell, isForward: boolean): FieldCell => {
+    const reduceFn = reduceRight;
+    const effectiveRowSegment = effectiveFieldRow.slice(0, cell.visibleCell.column);
+    return reduceFn(
+        effectiveRowSegment,
+        (effectiveCell, nextCell, index) => {
+            const nextCellIndex = index;
+            if (!nextCell) {
+                return {
+                    visibleCell: {
+                        ...effectiveCell.visibleCell,
+                        column: nextCellIndex
+                    },
+                    hiddenCells: effectiveCell.hiddenCells.map((cell) => ({ ...cell, column: nextCellIndex }))
+                };
+            } else if (!nextCell.visibleCell.merged && nextCell.visibleCell.value === effectiveCell.visibleCell.value) {
+                return {
+                    visibleCell: {
+                        ...effectiveCell.visibleCell,
+                        id: getNewId(),
+                        column: nextCellIndex,
+                        value: effectiveCell.visibleCell.value * 2,
+                        merged: true
+                    },
+                    hiddenCells: [
+                        nextCell.visibleCell,
+                        {
+                            ...effectiveCell.visibleCell,
+                            column: nextCellIndex
+                        }
+                    ]
+                };
+            }
+            return effectiveCell;
+        },
+        cloneDeep(cell)
+    );
+};
 
 const cellMapToField = (cellMap: CellDefinitionMap, fieldSize: number): Field => {
     const field = makeEmptyField(fieldSize);
@@ -162,7 +280,7 @@ const cellMapToField = (cellMap: CellDefinitionMap, fieldSize: number): Field =>
 };
 
 const fieldToCellMap = (field: Field): CellDefinitionMap => {
-    return reduce(
+    const result = reduce(
         field,
         (result: CellDefinitionMap, row: FieldRow) => {
             const rowCells = reduce(
@@ -182,7 +300,11 @@ const fieldToCellMap = (field: Field): CellDefinitionMap => {
         },
         {}
     );
+    console.log('fieldToCellMap', field, result);
+    return result;
 };
 
 const makeEmptyField = (fieldSize: number): Field =>
-    Array.from({ length: fieldSize }, () => Array.from({ length: fieldSize }, () => null));
+    Array.from({ length: fieldSize }, () => makeEmptyFieldRow(fieldSize));
+
+const makeEmptyFieldRow = (fieldSize: number): FieldRow => Array.from({ length: fieldSize }, () => null);
